a8dde357c2d8689b79419f29fa5095df9405fece
AzhahraRisky <azhahra.risky1@gmail.com>
2021-04-06 10:07:25 +0700

    web2

---
+++ minggu-1/praktikum/konstanta.php
@@ -0,0 +1,6 @@
+<?php
+define ("NAMA", "Blegug Cakranegara ");
+define ("NILAI", 90);
+echo "Nama : " . NAMA;
+echo "<br>Nilai : " . NILAI;
+?>
---
+++ minggu-1/praktikum/variabel.php
@@ -0,0 +1,6 @@
+<?php
+$nim = "A22.2020.02827";
+$nama = 'Blegug Cakranegara';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
